4fc44ccc0e99872d87e3e9ad06f5df1d551052ff
comedian73 <118024172+comedian73@users.noreply.github.com>
2023-05-09 22:07:41 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,15 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        Cat beancat = (Cat) applicationContext.getBean("cat");
+        Cat beancat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beancat.getName());
+        System.out.println(beancat1.getName());
+        System.out.println(bean.equals(bean1));
+        System.out.println(beancat.equals(beancat1));
     }
 }
